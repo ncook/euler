971896c12f605fae48455b32d487037a830621db
Neil Cook <20279+ncook@users.noreply.github.com>
2017-12-28 17:29:53 -0500

    Use error chain for cleaner Result handling
    Finally chose a way to handle Split and errors.

--- euler22/src/main.rs
+++ euler22/src/main.rs
@@ -1,11 +1,54 @@
-use std::io::BufRead;
-use std::io::BufReader;
-use std::fs::File;
+// `error_chain!` can recurse deeply
+#![recursion_limit = "1024"]
+
+// Import the macro. Don't forget to add `error-chain` in your
+// `Cargo.toml`!
+#[macro_use]
+extern crate error_chain;
+
+// We'll put our errors in an `errors` module, and other modules in
+// this crate will `use errors::*;` to get access to everything
+// `error_chain!` creates.
+mod errors {
+    // Create the Error, ErrorKind, ResultExt, and Result types
+    error_chain! { }
+}
+
+use errors::*;
 
 fn main() {
-    let mut names = Vec::new();
+    if let Err(ref e) = run() {
+        use std::io::Write;
+        let stderr = &mut ::std::io::stderr();
+        let errmsg = "Error writing to stderr";
 
-    let f = File::open("../names.txt").expect("Failed to open ../names.txt");
+        writeln!(stderr, "error: {}", e).expect(errmsg);
+
+        for e in e.iter().skip(1) {
+            writeln!(stderr, "caused by: {}", e).expect(errmsg);
+        }
+
+        // The backtrace is not always generated. Try to run this example
+        // with `RUST_BACKTRACE=1`.
+        if let Some(backtrace) = e.backtrace() {
+            writeln!(stderr, "backtrace: {:?}", backtrace).expect(errmsg);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
+    use std::fs::File;
+    use std::io::BufRead;
+    use std::io::BufReader;
+
+    let mut names = Vec::new();
+    const FNAME: &'static str = "../names.txt";
+
+    let f = File::open(FNAME)
+        .chain_err(|| "Failed to open name file")?;
+
     let reader = BufReader::new(f);
 
     for s in reader.split(b',') {
@@ -16,12 +59,9 @@ fn main() {
             Err(_) => panic!("Error reading file")
         }
     };
-    for s in reader.split(b',') {
-        s.map(|qs| qs[1..qs.len()].to_vec());
-    };
-    names.push(reader.split(b',').map(|qs| qs[1..qs.len()].to_vec()));
     // println!("There are {} amicable numbers less than {} and their sum is {}",
     // amicable.len(), LIMIT, amicable.into_iter().sum::<usize>());
+    Ok(())
 }
 
 #[cfg(test)]
